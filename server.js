6601f95305fbb52878fb06e3dd117a5ac8e35c6a
WEM90 <emo333f@gmail.com>
2025-06-05 23:33:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.post('/increment', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
